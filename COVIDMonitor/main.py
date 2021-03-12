b77ee6f5d48029b05c9ad658cb5f685f7c572d09
helenzzq <helenzhu0411@gmail.com>
2021-03-12 13:14:55 -0500

    add basic background img and upload button

--- COVIDMonitor/main.py
+++ COVIDMonitor/main.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask,render_template
 
-app = Flask("Assignment 2")
 
-@app.route('/monitor')
-def welcome_monitor():
-	return 'Welcome to the Covid Monitor!'
+app = Flask(__name__)
+
+@app.route('/')
+def main():
+	return render_template('index.html')
 
 if __name__ == "__main__":
-	app.run()
+	app.run(debug=True)
